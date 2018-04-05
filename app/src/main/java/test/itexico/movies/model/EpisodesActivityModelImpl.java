db7163eb7493b69fc79d776bb85e8a6dcc4f558b
Alberto-Simonin <alberto.simonin@hotmail.com>
2018-04-05 16:27:25 -0500

    Refactor headers de peticiones

--- app/src/main/java/test/itexico/movies/model/EpisodesActivityModelImpl.java
+++ app/src/main/java/test/itexico/movies/model/EpisodesActivityModelImpl.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import test.itexico.movies.managers.RequestManager;
+import test.itexico.movies.managers.StandardRequest;
 import test.itexico.movies.utils.Trakt;
 
 public class EpisodesActivityModelImpl implements EpisodesActivityModel {
@@ -27,17 +28,7 @@ public class EpisodesActivityModelImpl implements EpisodesActivityModel {
     public void getData(long seasonId, Response.Listener<JSONArray> onResponse, Response.ErrorListener onError) {
         RequestManager requestManager = RequestManager.getInstance(context);
         String url = Trakt.getEpisodesURL(seasonId+"");
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, onResponse, onError){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json");
-                headers.put("trackt-api-version", "2");
-                headers.put("trackt-api-key", Trakt.getClientID());
-                return headers;
-            }
-        };
-
+        StandardRequest request = new StandardRequest(Request.Method.GET, url, null, onResponse, onError);
         requestManager.addToRequestQueue(request);
     }
 }
